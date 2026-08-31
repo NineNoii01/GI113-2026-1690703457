13f5f0464c9a1eef74c70bbb910f3c0a2ca1ac7f
NineNoii01 <kittipop.m08092549@gmail.com>
2026-08-31 09:51:06 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -54,17 +54,14 @@ namespace Lab02
             int currentHp1 = 190;
             float attackPower1 = 50.0f;
             double critMultiplier1 = 1.5;
-            bool isBoss1 = false;
 
-            Console.WriteLine($"Name: {Name1}");
-            Console.WriteLine($"Rank: {rank1}");
-            Console.WriteLine($"Level: {level1}");
-            Console.WriteLine($"HP: {currentHp1} / {maxHp1}");
-            Console.WriteLine($"Attack Power: {attackPower1}");
-            Console.WriteLine($"Crit Multiplier: {critMultiplier1}");
-            Console.WriteLine($"Is Boss: {isBoss1}");
-            Console.WriteLine();
-
+            Console.WriteLine($"Name: {Name1}" +
+                $"\nRank: {rank1}" +
+                $"\nLevel: {level1}" +
+                $"\nHP: {currentHp1} / {maxHp1}" +
+                $"\nAttack Power: {attackPower1}" +
+                $"\nCrit Multiplier: {critMultiplier1}\n");
+            
             string Name2 = "Not";
             char rank2 = 'S';
             int level2 = 5;
@@ -72,16 +69,13 @@ namespace Lab02
             int currentHp2 = 175;
             float attackPower2 = 35.0f;
             double critMultiplier2 = 1.5;
-            bool isBoss2 = false;
 
-            Console.WriteLine($"Name: {Name2}");
-            Console.WriteLine($"Rank: {rank2}");
-            Console.WriteLine($"Level: {level2}");
-            Console.WriteLine($"HP: {currentHp2} / {maxHp2}");
-            Console.WriteLine($"Attack Power: {attackPower2}");
-            Console.WriteLine($"Crit Multiplier: {critMultiplier2}");
-            Console.WriteLine($"Is Boss: {isBoss2}");
-            Console.WriteLine();
+            Console.WriteLine($"Name: {Name2}" +
+                $"\nRank: {rank2}" +
+                $"\nLevel: {level2}" +
+                $"\nHP: {currentHp2} / {maxHp2}" +
+                $"\nAttack Power: {attackPower2}" +
+                $"\nCrit Multiplier: {critMultiplier2}\n");
 
             string Name3 = "Bank";
             char rank3 = 'A';
@@ -90,16 +84,13 @@ namespace Lab02
             int currentHp3 = 150;
             float attackPower3 = 20.0f;
             double critMultiplier3 = 1.5;
-            bool isBoss3 = false;
 
-            Console.WriteLine($"Name: {Name3}");
-            Console.WriteLine($"Rank: {rank3}");
-            Console.WriteLine($"Level: {level3}");
-            Console.WriteLine($"HP: {currentHp3} / {maxHp3}");
-            Console.WriteLine($"Attack Power: {attackPower3}");
-            Console.WriteLine($"Crit Multiplier: {critMultiplier3}");
-            Console.WriteLine($"Is Boss: {isBoss3}");
-            Console.WriteLine();
+            Console.WriteLine($"Name: {Name3}" +
+                $"\nRank: {rank3}" +
+                $"\nLevel: {level3}" +
+                $"\nHP: {currentHp3} / {maxHp3}" +
+                $"\nAttack Power: {attackPower3}" +
+                $"\nCrit Multiplier: {critMultiplier3}\n");
 
             string Name4 = "Ken";
             char rank4 = 'B';
@@ -108,16 +99,13 @@ namespace Lab02
             int currentHp4 = 100;
             float attackPower4 = 15.0f;
             double critMultiplier4 = 1.5;
-            bool isBoss4 = false;
 
-            Console.WriteLine($"Name: {Name4}");
-            Console.WriteLine($"Rank: {rank4}");
-            Console.WriteLine($"Level: {level4}");
-            Console.WriteLine($"HP: {currentHp4} / {maxHp4}");
-            Console.WriteLine($"Attack Power: {attackPower4}");
-            Console.WriteLine($"Crit Multiplier: {critMultiplier4}");
-            Console.WriteLine($"Is Boss: {isBoss4}");
-            Console.WriteLine();
+            Console.WriteLine($"Name: {Name4}" +
+                $"\nRank: {rank4}" +
+                $"\nLevel: {level4}" +
+                $"\nHP: {currentHp4} / {maxHp4}" +
+                $"\nAttack Power: {attackPower4}" +
+                $"\nCrit Multiplier: {critMultiplier4}\n");
 
         }
     }
