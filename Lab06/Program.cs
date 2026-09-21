6edade84f18ade3bd30f8a0c50348602dc3d514b
NineNoii01 <kittipop.m08092549@gmail.com>
2026-09-21 10:51:51 +0700

    Lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int lives = 0;
+
+            //if (lives == 0) // ด้านในวงเล็บ ต้องมีค่าเป็น Boolean
+            //{
+            //    Console.WriteLine("Game Over");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continue Running");
+            //}
+
+            ////2
+            //int coins = 80;
+            //int price = 100;
+
+            //if (coins >= price)
+            //{
+            //    Console.WriteLine("Purchased");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not enough coin.");
+            //}
+
+            ////3
+            //int score = 75;
+
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("Rank S");
+            //}
+            //else if (score >= 60)
+            //{
+            //    Console.WriteLine("Rank A");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Rank B");
+            //}
+
+            ////4
+            //bool haskey = true;
+            //Console.Write("Your Level (1-99): ");
+            //bool ok = int.TryParse(Console.ReadLine(), out int level);
+
+            //if (!ok || level < 1 || level > 99)
+            //{
+            //    Console.WriteLine("Invalid Level, please try agin");
+            //}
+            //else if (level >= 10 && haskey)
+            //{
+            //    Console.WriteLine("Boss floor unlocked.");
+            //}
+            //else if (level >= 5)
+            //{
+            //    Console.WriteLine("Boss floor unlocked.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The door stays shut.");
+            //}
+
+            Console.WriteLine("");
+
+        }
+    }
+}
